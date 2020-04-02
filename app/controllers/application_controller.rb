59fbbbe3e45a47dd5d834110033052f7ce99e4b4
Raymond Usbal <raymond@philippinedev.com>
2020-04-02 10:08:58 +0800

    check user session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :check_user_session
+
+ private
+
+  def check_user_session
+    return if controller_name == 'signin'
+    return unless session[:user_id].nil?
+
+    redirect_to controller: 'signin', action: 'new'
+  end
 end
